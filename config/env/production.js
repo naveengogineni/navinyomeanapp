e636f291704d6cfc787b1702e9ca5d56eaacb15b
Naveen Gogineni <gogineni.naveen@gmail.com>
2014-07-18 23:38:21 +0530

    Update production.js

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://naveen:naveen@kahana.mongohq.com:10063/navinyomeanapp',
+	db: process.env.MONGOHQ_URL || process.env.MONGOHQ_URI || 'mongodb://naveen:naveen@kahana.mongohq.com:10063/navinyomeanapp',
 	assets: {
 		lib: {
 			css: [
